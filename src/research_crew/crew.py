f641d4e2fc7a6c5dc94e6054ac7a6990487aa32d
OneDuckyBoy <stilianmatev@gmail.com>
2025-03-23 15:25:35 +0200

    code for agents for instagram agents before testing if it works

--- src/research_crew/crew.py
+++ src/research_crew/crew.py
@@ -5,7 +5,8 @@ from crewai.project import CrewBase, agent, crew, task
 # from research_crew.tools.custom_tool import MyCustomTool
 
 # Check our tools documentations for more information on how to use them
-# from crewai_tools import SerperDevTool
+from crewai_tools import FileReadTool, TXTSearchTool, DallETool, WebSearchTool, OutputFileTool, TextProcessingTool
+
 
 @CrewBase
 class ResearchCrewCrew():
@@ -73,7 +74,70 @@ class ResearchCrewCrew():
 
 
 
+    # Intagram agents setup
+    @agent
+    def instagram_supervisor_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['instagram_supervisor_agent'],
+            verbose=True
+        )
 
+    @agent
+    def instagram_writer_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['instagram_writer_agent'],
+            verbose=True
+        )
+
+    @agent
+    def instagram_editor_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['instagram_editor_agent'],
+            verbose=True
+        )
+
+    @agent
+    def instagram_image_prompt_agent(self) -> Agent:
+        dalle_tool = DallETool(
+            model="dall-e-3",
+            size="1024x1024",
+            quality="standard",
+            n=1
+        )   
+        return Agent(
+            config=self.agents_config['instagram_image_prompt_agent'],
+            tools=[dalle_tool],
+            verbose=True
+        )
+
+    # Instagram tools tasks setup
+    @task
+    def instagram_supervise_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['instagram_supervise_task'],
+            agent=self.supervisor_agent()
+        )
+
+    @task
+    def instagram_write_post_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['instagram_write_post_task'],
+            agent=self.writer_agent()
+        )
+
+    @task
+    def instagram_edit_post_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['instagram_edit_post_task'],
+            agent=self.editor_agent()
+        )
+
+    @task
+    def instagram_generate_image_prompt_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['instagram_generate_image_prompt_task'],
+            agent=self.image_prompt_agent()
+        )
 
     @crew
     def crew(self) -> Crew:
@@ -81,52 +145,7 @@ class ResearchCrewCrew():
         return Crew(
             agents=self.agents,  # Automatically created by the @agent decorator
             tasks=self.tasks,  # Automatically created by the @task decorator
-            process=Process.sequential,
+            # process=Process.sequential,
             verbose=2,
             process=Process.hierarchical, # Uncomment to use hierarchical process
         )
-    # @agent
-    # def researcher(self) -> Agent:
-    #     # Define the tools you need : )
-    #     file_read_tool = FileReadTool(file_path='./Data/marketing information.txt')
-    #     semantic_search_tool = TXTSearchTool(txt='./Data/marketing information.txt')
-
-    #     return Agent(
-    #         config=self.agents_config['researcher'],
-    #         tools=[file_read_tool, semantic_search_tool],  # Include the tools here
-    #         verbose=True
-    #     )
-
-    # @task
-    # def research_task(self) -> Task:
-    #     return Task(
-    #         config=self.tasks_config['research_task'],
-    #         agent=self.researcher()
-    #     )
-    
-
-    # @agent
-    # def trend_analyst(self) -> Agent:
-    #     return Agent(
-    #         config=self.agents_config['trend_analyst'],
-    #         verbose=True
-    #     )
-    # @agent
-    # def reporting_analyst(self) -> Agent:
-    #     return Agent(
-    #         config=self.agents_config['reporting_analyst'],
-    #         verbose=True
-    #     )
-    
-
-    
-
-    # @task
-    # def reporting_task(self) -> Task:
-    #     return Task(
-    #         config=self.tasks_config['reporting_task'],
-    #         agent=self.reporting_analyst(),
-    #         output_file='report.md'
-    #     )
-
-    
--- src/research_crew/main.py
+++ src/research_crew/main.py
@@ -3,8 +3,7 @@ from research_crew.crew import ResearchCrewCrew
 
 
 def run():
-    # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
-        'topic': 'AI LLMs'
+
     }
     ResearchCrewCrew().crew().kickoff(inputs=inputs)
